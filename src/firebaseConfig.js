ec059f1866ab3cd8b23171e64e47fe3063bf116d
Skye <55710846+leapcoded@users.noreply.github.com>
2025-09-23 09:04:25 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,11 +3,5 @@
 // Copy the firebaseConfig object and paste it here.
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBQi0Z3GkQp8r7PSOPQstcnp8QsIk8ZjIs",
-  authDomain: "the-haven-wiki.firebaseapp.com",
-  projectId: "the-haven-wiki",
-  storageBucket: "the-haven-wiki.firebasestorage.app",
-  messagingSenderId: "17806292737",
-  appId: "1:17806292737:web:3e1e3b1be83d57b3bf8d00",
-  measurementId: "G-GWY16R06TM"
+
 };
